3fc777c3a6e5844d2f9ae04731b29ccf8697f524
phanuwit13 <big.phanuwit@gmail.com>
2023-03-20 15:45:39 +0700

    add detail page

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -47,7 +47,7 @@ const Detail = () => {
   }, [])
 
   return (
-    <div className='w-[90%] max-w-[700px] m-auto '>
+    <div className='w-[90%] max-w-[600px] m-auto '>
       <div className='flex m-auto w-fit '>
         <img
           src='/images/logo.webp'
@@ -58,13 +58,14 @@ const Detail = () => {
       <div className='my-[20px]'>
         <Link
           to='/'
-          className=' bg-gray-200 px-2 pr-4 rounded-[4px] flex items-center w-fit'
+          className='bg-[#253641] px-2 pr-4 rounded-[4px] flex items-center w-fit'
         >
           <svg
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 16 16'
             width='16'
             height='16'
+            className='fill-white'
           >
             <path d='M9.78 12.78a.75.75 0 0 1-1.06 0L4.47 8.53a.75.75 0 0 1 0-1.06l4.25-4.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042L6.06 8l3.72 3.72a.75.75 0 0 1 0 1.06Z'></path>
           </svg>
@@ -74,34 +75,19 @@ const Detail = () => {
       {pokemonDetail.data && (
         <div className='!z-5 relative flex justify-self-center rounded-[20px]  bg-clip-border shadow-3xl shadow-shadow-500 flex-col w-full !p-4 3xl:p-![18px]  undefined'>
           <div className='h-full w-full'>
-            <div className='relative w-full aspect-square'>
-              <img
-                src={pokemonDetail.data?.image}
-                className='mb-3 w-full rounded-xl 3xl:h-full 3xl:w-full h-[218px] p-[20px] absolute z-20'
-                alt=''
-              />
-              <img src="\images\pokemon_bg.png" className="absolute top-0 h-[400px]" alt="" />
-              <button className='absolute top-0 right-0 flex items-center justify-center rounded-full bg-white p-2 text-brand-500 hover:cursor-pointer'>
-                <div className='flex h-full w-full items-center justify-center rounded-full text-xl hover:bg-gray-50'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    strokeWidth='0'
-                    viewBox='0 0 512 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      fill='none'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='32'
-                      d='M352.92 80C288 80 256 144 256 144s-32-64-96.92-64c-52.76 0-94.54 44.14-95.08 96.81-1.1 109.33 86.73 187.08 183 252.42a16 16 0 0018 0c96.26-65.34 184.09-143.09 183-252.42-.54-52.67-42.32-96.81-95.08-96.81z'
-                    ></path>
-                  </svg>
-                </div>
-              </button>
+            <div className=' w-full'>
+              <div className='relative w-[100%] h-[400px]'>
+                <img
+                  src='\images\pokemon_bg.png'
+                  className='absolute h-[auto] max-h-[400px] translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]'
+                  alt=''
+                />
+                <img
+                  src={pokemonDetail.data?.image}
+                  className='mb-3 w-full rounded-xl 3xl:h-full 3xl:w-full h-[180px] sm:h-[218px] p-[20px] absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]'
+                  alt=''
+                />
+              </div>
             </div>
             <div className='mb-3 flex items-center justify-between px-1 md:items-start'>
               <div className='mb-2'>
@@ -115,17 +101,60 @@ const Detail = () => {
                 </p>
               </div>
             </div>
-            <div className='flex items-center justify-end md:items-center lg:justify-end gap-[6px]'>
-              {pokemonDetail.data?.types.map((v) => {
-                return (
-                  <span
-                    key={`badge-type-${v.slot}`}
-                    className={`min-w-[70px] rounded-[20px] px-3 py-1 text-base font-medium text-[14px] badge-type-${v.type.name} text-center`}
-                  >
-                    {v.type.name}
-                  </span>
-                )
-              })}
+
+            <div className='bg-[#253641] grid grid-cols-1 sm:grid-cols-2 w-[100%] mt-[20px] rounded-[12px] p-[16px] gap-y-[20px] gap-x-[30px]'>
+              <div className=''>
+                <div className='flex gap-[16px]'>
+                  <div className='text-[#b3eafe]'>Height</div>
+                  <div>{(pokemonDetail.data.height / 10).toFixed(2)} m</div>
+                </div>
+                <div className='flex gap-[16px]'>
+                  <div className='text-[#b3eafe]'>Weight</div>
+                  <div>{(pokemonDetail.data.weight / 10).toFixed(2)} kg.</div>
+                </div>
+              </div>
+              <div className='flex justify-start sm:justify-end gap-[6px] h-fit'>
+                {pokemonDetail.data.types.map((v) => {
+                  return (
+                    <span
+                      key={`badge-type-${v.slot}`}
+                      className={`min-w-[70px] rounded-[20px] px-3 py-1 text-black text-base font-medium text-[14px] badge-type-${v.type.name} text-center`}
+                    >
+                      {v.type.name}
+                    </span>
+                  )
+                })}
+              </div>
+              <div className='w-[100%] h-fit'>
+                <h2>Abilities</h2>
+                <div className='grid grid-cols-2 sm:grid-cols-1 gap-[10px] flex-col mt-[10px]'>
+                  {pokemonDetail.data.abilities.map((ability) => {
+                    return (
+                      <div
+                        key={`ability-${ability.ability}`}
+                        className={`min-w-[70px] capitalize rounded-[20px] px-3 py-1 text-black text-base font-medium text-[14px] bg-[#b3eafe] text-center`}
+                      >
+                        {ability.ability.name}
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+              <div className='w-[100%] h-fit'>
+                <h2>State</h2>
+                <div className='flex gap-[10px] flex-col mt-[10px]'>
+                  {pokemonDetail.data.stats.map((state) => {
+                    return (
+                      <div className='flex gap-[16px] justify-between'>
+                        <div className='text-[#b3eafe] capitalize'>
+                          {state.stat.name}
+                        </div>
+                        <div>{state.base_stat}</div>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
             </div>
           </div>
         </div>
